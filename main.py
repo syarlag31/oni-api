91a692582eef8dbaba0344e42f6e9318430a1baa
Sathwik Yarlagadda <54608608+syarlag31@users.noreply.github.com>
2023-05-29 06:32:45 -0400

    Version 0.5

---
+++ main.py
@@ -0,0 +1,33 @@
+from fastapi import FastAPI, HTTPException, Request
+from dotenv import load_dotenv
+import boto3, os, json
+
+load_dotenv()
+app = FastAPI()
+
+@app.get('/test')
+def get_root():
+    return {'Hi': 'There!'}
+
+@app.post("/webhook")
+async def webhook(request: Request):
+    try:
+        data = await request.json()
+        encoded_data = json.dumps(data)
+    
+        ACCESS_KEY = os.getenv('ACCESS_KEY')
+        SECRET_KEY = os.getenv('SECRET_KEY')
+        region = 'us-east-2'
+
+        lambda_client = boto3.client('lambda', aws_access_key_id=ACCESS_KEY,
+                                    aws_secret_access_key=SECRET_KEY,
+                                    region_name=region)
+        
+        response = lambda_client.invoke(
+            FunctionName='testfunction',
+            InvocationType='Event',  # Use 'Event' for asynchronous invocation
+            Payload=f'{encoded_data}'  # Optional payload data to pass to the Lambda function
+        )
+        
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
