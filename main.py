f9d135ad4262159db0ab1b42da8c19801e8f2d4a
Sathwik Yarlagadda <54608608+syarlag31@users.noreply.github.com>
2023-05-29 21:06:28 -0400

    Adding webhook alert

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, HTTPException, Request
 from dotenv import load_dotenv
-import boto3, os, json
+import boto3, os, json, requests
 
 load_dotenv()
 app = FastAPI()
@@ -40,3 +40,40 @@ async def webhook(request: Request):
         
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
+
+@app.post("/alert")
+async def format_and_post(json_data: dict):
+    # Extract data from the input JSON
+    ticker = json_data.get("ticker")
+    color = json_data.get("color")
+    tp = json_data.get("TP")
+    stop_loss = json_data.get("stopLoss")
+    timestamp = json_data.get("timestamp")
+    
+    # Format the data into the desired JSON structure
+    formatted_json = {
+        "content": None,
+        "embeds": [
+            {
+                "title": f"Sell Alert {ticker}",
+                "url": f"https://www.tradingview.com/symbols/{ticker}/",
+                "color": color,
+                "fields": [
+                    {
+                        "name": "Sell",
+                        "value": str(stop_loss)
+                    }
+                ],
+                "footer": {
+                    "text": "Free Beta Algo v1.0.0"
+                },
+                "timestamp": timestamp
+            }
+        ]
+    }
+    
+    # Post the formatted JSON to the Discord webhook URL
+    webhook_url = "https://discord.com/api/webhooks/1112897541919490190/8QpJ9Qxaw4eZR2BcU-hx4gtcFs-yUPZaBoHRT3Zjm21bvoAg3jsBSb3oea_ZmyfWb4gX"
+    response = requests.post(webhook_url, json=formatted_json)
+    
+    return {"status": response.status_code}
