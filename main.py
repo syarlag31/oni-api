88e19280f48454c397bef22b821dc52c843a0804
Sathwik Yarlagadda <sathwik.y31@gmail.com>
2023-05-31 21:59:02 -0400

    Updating API

--- main.py
+++ main.py
@@ -3,43 +3,42 @@ from dotenv import load_dotenv
 import boto3, os, json, requests
 
 load_dotenv()
-app = FastAPI()
+app = FastAPI(
+    #docs_url=None, # Disable docs (Swagger UI) for PROD
+    #redoc_url=None, # Disable redoc for PROD
+)
 
-@app.get('/test')
-def get_root():
-    return {'Hi': 'There!'}
+# @app.post("/webhook")
+# async def webhook(request: Request):
+#     try:
+#         try:
+#             data = await request.json()
+#         except json.JSONDecodeError:
+#             try:
+#                 # Attempt to convert the string into JSON
+#                 data = json.loads(await request.body())
+#             except json.JSONDecodeError:
+#                 raise HTTPException(status_code=400, detail="Invalid JSON data")
 
-@app.post("/webhook")
-async def webhook(request: Request):
-    try:
-        try:
-            data = await request.json()
-        except json.JSONDecodeError:
-            try:
-                # Attempt to convert the string into JSON
-                data = json.loads(await request.body())
-            except json.JSONDecodeError:
-                raise HTTPException(status_code=400, detail="Invalid JSON data")
-
-        data = await request.json()
-        encoded_data = json.dumps(data)
+#         data = await request.json()
+#         encoded_data = json.dumps(data)
     
-        ACCESS_KEY = os.getenv('ACCESS_KEY')
-        SECRET_KEY = os.getenv('SECRET_KEY')
-        region = 'us-east-2'
+#         ACCESS_KEY = os.getenv('ACCESS_KEY')
+#         SECRET_KEY = os.getenv('SECRET_KEY')
+#         region = 'us-east-2'
 
-        lambda_client = boto3.client('lambda', aws_access_key_id=ACCESS_KEY,
-                                    aws_secret_access_key=SECRET_KEY,
-                                    region_name=region)
+#         lambda_client = boto3.client('lambda', aws_access_key_id=ACCESS_KEY,
+#                                     aws_secret_access_key=SECRET_KEY,
+#                                     region_name=region)
         
-        response = lambda_client.invoke(
-            FunctionName='testfunction',
-            InvocationType='Event',  # Use 'Event' for asynchronous invocation
-            Payload=encoded_data  # Optional payload data to pass to the Lambda function
-        )
+#         response = lambda_client.invoke(
+#             FunctionName='testfunction',
+#             InvocationType='Event',  # Use 'Event' for asynchronous invocation
+#             Payload=encoded_data  # Optional payload data to pass to the Lambda function
+#         )
         
-    except Exception as e:
-        raise HTTPException(status_code=400, detail=str(e))
+#     except Exception as e:
+#         raise HTTPException(status_code=400, detail=str(e))
 
 @app.post("/alert")
 async def format_and_post_to_discord(data: dict):
@@ -94,6 +93,7 @@ async def format_and_post_to_discord(data: dict):
     }
 
     try:
+        # Sends request to the URL below. Currently static and works for only one URL that can be replaced below
         response = requests.post(
             "https://discord.com/api/webhooks/1112897541919490190/8QpJ9Qxaw4eZR2BcU-hx4gtcFs-yUPZaBoHRT3Zjm21bvoAg3jsBSb3oea_ZmyfWb4gX",
             json=formatted_json
