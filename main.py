b4987c32a939251cb579656380c7746f1fc454e9
Sathwik Yarlagadda <54608608+syarlag31@users.noreply.github.com>
2023-06-05 15:37:09 -0400

    Fixed some bugs

--- main.py
+++ main.py
@@ -23,21 +23,39 @@ api_key_header = APIKeyHeader(name="X-API-Key", auto_error=False)
 class ConnectionManager:
     def __init__(self):
         self.active_connections: Dict[str, WebSocket] = {}
+        self.user_connections: Dict[str, str] = {}
 
-    async def connect(self, websocket: WebSocket, session_token: str):
+    async def connect(self, websocket: WebSocket, session_token: str, user_id: str):
         await websocket.accept()
+        if user_id in self.user_connections:  # User has an existing session
+            old_token = self.user_connections[user_id]
+            await self.check_and_close(old_token)
         self.active_connections[session_token] = websocket
+        self.user_connections[user_id] = session_token
         print(self.active_connections)
 
     async def disconnect(self, session_token: str):
         websocket = self.active_connections.get(session_token)
         if websocket:
-            await websocket.close()
+            try:
+                await websocket.close()
+            except RuntimeError:
+                pass  # The socket was already closed
             del self.active_connections[session_token]
 
     async def send_json(self, data: str):
         for websocket in self.active_connections.values():
             await websocket.send_json(data)
+
+    async def check_and_close(self, old_token: str):
+        if old_token in self.active_connections:
+            websocket = self.active_connections[old_token]
+            if websocket:
+                try:
+                    await websocket.close()
+                except RuntimeError:
+                    pass  # The socket was already closed
+                del self.active_connections[old_token]
 
 manager = ConnectionManager()
 
@@ -59,11 +77,12 @@ def generate_session_token():
 @app.post("/session-token")
 async def get_session_token(user=Depends(authenticate_user)):
     session_token = generate_session_token()
+    user_id = str(user[0]) # Convert UUID to string
 
     try:
         cursor = connection.cursor()
         query = "UPDATE users SET session_token = %s WHERE id = %s"
-        cursor.execute(query, (session_token, str(user[0])))  # Convert UUID to string
+        cursor.execute(query, (session_token, user_id))
         connection.commit()
         cursor.close()
 
@@ -75,7 +94,7 @@ async def get_session_token(user=Depends(authenticate_user)):
 @app.websocket("/ws/{session_token}")
 async def websocket_endpoint(websocket: WebSocket, session_token: str):
     cursor = connection.cursor()
-    query = "SELECT session_token FROM users WHERE session_token = %s"
+    query = "SELECT id, session_token FROM users WHERE session_token = %s"
     cursor.execute(query, (session_token,))
     session = cursor.fetchone()
     cursor.close()
@@ -83,11 +102,13 @@ async def websocket_endpoint(websocket: WebSocket, session_token: str):
     if session is None:
         await websocket.close(code=1008)
         return
-    
-    await manager.connect(websocket, session_token)
+
+    user_id, session_token = str(session[0]), session[1]  # Convert UUID to string
+    await manager.connect(websocket, session_token, user_id)
     try:
         while True:
             data = await websocket.receive_text()
+            # Add message verification
     except WebSocketDisconnect:
         await manager.disconnect(session_token)
 
